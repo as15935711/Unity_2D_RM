c45df80676d3226386c36bbad20fd6b4fc947117
as15935711 <82298466+as15935711@users.noreply.github.com>
2021-04-25 13:02:14 +0800

    C#基本介紹

---
+++ 2D_RM/Assets/Scripts/move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    [Header("汽車的 cc 數")]
+    [Tooltip("這是汽車的 cc 數")]
+    [Range(1000,5000)]
+    public int cc = 2000;
+    [Header("汽車的重量"), Tooltip("這是汽車的重量。"), Range(0.5f,10)]
+    public float weight = 1.5f;
+    [Header("汽車的品牌")]
+    public string brand = "BMW";
+    [Header("有沒有天窗")]
+    public bool hasWindow =true ;
+
+
+    public Color color;
+    public Color red = Color.red;
+    public mycolor =now Color(0.3, 0 ,0.6f);
+
+
+}
