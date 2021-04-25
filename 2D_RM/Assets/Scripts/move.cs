ac4204431f7a981f6488c6ea7a77e7e0b41aa199
as15935711 <82298466+as15935711@users.noreply.github.com>
2021-04-25 15:25:07 +0800

    設定

--- 2D_RM/Assets/Scripts/move.cs
+++ 2D_RM/Assets/Scripts/move.cs
@@ -18,7 +18,30 @@ public class move : MonoBehaviour
 
     public Color color;
     public Color red = Color.red;
-    public mycolor =now Color(0.3, 0 ,0.6f);
+    public Color mycolor =new Color(0.3f, 0 ,0.6f);
+    public Color mycolor2 = new Color(0, 0.5f, 0.5f, 0.5f);
 
+    //座標二維-四維 Vector2 Vector3 Vector4
+    public Vector2 v2;
+    public Vector2 v2zero = Vector2.zero;
+    public Vector2 v2one = Vector2.one;
+    public Vector2 v2my = new Vector2(7, 9);
+
+    public Vector3 v3 = new Vector3(1, 2, 3);
+    public Vector4 v4 = new Vector4(1, 2, 3, 4);
+
+    public KeyCode key1;
+    public KeyCode key2 = KeyCode.A;      //不指定為 None (無)
+    public KeyCode key3 = KeyCode.Mouse0; //左0 右1 滾輪2
+    public KeyCode key4 = KeyCode.Joystick1Button0;
+
+    //遊戲物件 與元件
+    //遊戲物件 GameObject
+    public GameObject obj1;
+    public GameObject obj2;
+    //元件 Component - 屬性面板上可摺疊的
+    public Transform tra;
+    public SpriteRenderer spr;
 
 }
+ 
