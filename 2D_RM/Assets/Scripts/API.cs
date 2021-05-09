b3c97a9dc9db4f1561abe31ddbe7e82cac4aecd0
as15935711 <82298466+as15935711@users.noreply.github.com>
2021-05-09 13:03:56 +0800

    API

---
+++ 2D_RM/Assets/Scripts/API.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class API : MonoBehaviour
+{
+    //靜態API
+    //非靜態API
+
+    // 1. 非靜態 API 需要物件
+    // 2. 定義一個欄位 - 物件
+    public Transform tra1;
+    public Transform tra2;
+    public SpriteRenderer spr;
+
+    private void Start()
+    {
+        //靜態
+        //類別名稱.靜態屬性
+        float f = Random.value;
+
+        // 非靜態
+        //物件名稱.非靜態屬性
+        print("取得物件的座標:" + tra1.position);
+
+        //存放屬性 
+        //物件名稱.非靜態屬性 指定 值 ;
+        tra2.localScale = new Vector3(3, 3, 3);
+
+        spr.color = new Color(1, 0, 0);
+        spr.flipX = true;
+        
+    }
+
+    private void Update()
+    {
+        //非靜態
+        //使用方法 
+        //物件名稱.非靜態方法(對應參數
+        tra2.Translate(0.01f,0,0);
+    }
+}
